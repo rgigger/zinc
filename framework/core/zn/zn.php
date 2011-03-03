582f0c42166fc2528ad1ec51152f4a97463e8307
Rick Gigger <rick@Lutz.local>
2011-03-02 19:37:02 -0700

    tiny updates to zn.php

--- framework/core/zn/zn.php
+++ framework/core/zn/zn.php
@@ -1,6 +1,19 @@
 <?php
+// get the location of the init.php file according to the following rules
+// 1. if there is one in the current directory then use it
+// 2. check for a -D flag. If it is set then use that as the directory
+// 3. check for a -I flag. If it is set then check for the user config to see if
+//		there is an instance name and directory set up.  If so use that
+// 4. if there is an environmental variable INSTANCE_DIR set then use that
+
+// 2-4 above not done
+
 // if we are in an instance load zinc through the instance
 // other wise load it through
+
+// //	don't create the instance or the app, we just want bare bones functionality
+// define('app_simple', true);
+
 if(file_exists('./init.php'))
 	include 'init.php';
 else
@@ -54,7 +67,7 @@ if(defined('instance_dir'))
 	echo "zn COMMAND [ARGS]\n";
 else
 {
-	echo "usage: zn create instance INSTANCE_NAME\n";
+	echo "usage: zn create instance INSTANCE_NAME APP_DIR\n";
 	// echo "   create app APP_NAME\n";
 	// echo "   create module MODULE_NAME\n";
 	echo "\n";
