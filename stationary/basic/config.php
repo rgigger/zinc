1a425a7cf919420da4c20a6dec7f6a2cd4da919e
Rick Gigger <rick@Lutz.local>
2011-03-02 19:02:23 -0700

    Update the basic stationary to conform to the new format

--- stationary/basic/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('zoop_dir', '/path/to/zoop/framework');
-define('app_dir', dirname(__file__));
--- stationary/basic/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('config.php');
-include('includes.php');
-
-ZoneApplication::handleRequest();
