f6cbadc1821a8685b4c80ba7f7da86972aaa6d83
Rick Gigger <rgigger@gmail.com>
2012-08-28 20:21:08 -0600

    use the new configuration stuff

--- framework/core/build/BuildModule.php
+++ framework/core/build/BuildModule.php
@@ -1,13 +1,18 @@
 <?php
 class BuildModule extends ZoopModule
 {
+	protected function init()
+	{
+		$this->addClass('BuildProject');
+	}
+	
 	function getIncludes()
 	{
 		return array('functions.php');
 	}
 	
-	function getClasses()
-	{
-		return array('BuildProject');
-	}	
+	// function getClasses()
+	// {
+	// 	return array('BuildProject');
+	// }
 }
