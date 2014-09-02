7550da241535d7bbc520d8c4a3ca3270793e444e
Rick Gigger <rgigger@gmail.com>
2014-09-02 16:44:45 -0600

    allow for silent zn ex scripts

--- framework/core/zn/commands/CommandScript.php
+++ framework/core/zn/commands/CommandScript.php
@@ -18,7 +18,15 @@ class CommandScript
 		
 		$command = $p[2];
 		$filepath = app_dir . "/bin/$command.php";
-		echo "executing $filepath\n";
+		
+		// add a -v switch that can be added between the "zn ex" and the script name that will not be passed into the script
+		// this will necessitate altering what gets passed into the script so it doesn't contain any of the cruft it gets now
+		// that will probaly necessitate altering nearly every script you've ever made with zinc
+		// right now we just need to delete it though because otherwise we can't use it to create scripts whos output
+		// needs to be piped into other scripts
+		//
+		// echo "executing $filepath\n";
+		
 		if(file_exists($filepath))
 			include $filepath;
 		else
