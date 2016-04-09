5ca15383ac90ff341fa132e713f4ea43a118806e
Rick Gigger <rick@jeff.local>
2016-04-09 11:48:52 -0600

    include app_dir/domain in import path

--- py/pyzn.py
+++ py/pyzn.py
@@ -1,12 +1,20 @@
-# print(os.getcwd())
+# for some reason that I don't understand at all this is somehow the instance directrly
+sys.path.append(os.getcwd())
+import sys
+from const import *
 
-constScript = os.getcwd() + '/const.py'
-exec(open(constScript).read())
-# print(app_dir)
+sys.path.append(os.path.abspath(app_dir + '/domain'))
+
+
+print("app_dir = " + app_dir)
+print("instance_dir = " + instance_dir)
 # print(sys.argv)
 commandName = sys.argv[1]
 # print(commandName)
-assert(commandName == 'ex')
+# assert(commandName == 'ex')
+if commandName != "ex":
+    print('pyzn currently only handles one command: "ex"')
+    sys.exit(1)
 scriptName = sys.argv[2]
 # print(scriptName)
 scriptPath = app_dir + '/bin/' + scriptName + '.py'
